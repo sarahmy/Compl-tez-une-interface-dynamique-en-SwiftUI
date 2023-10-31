e46be508363c4d080d5859838ef4307a5b272728
Amandine Cousin <cousin.amandine.pro@gmail.com>
2023-10-31 11:50:54 +0100

    Initial commit : Ajout readme + gitignore / Creation du projet XCode

---
+++ README.md
@@ -0,0 +1,7 @@
+# Complétez une interface dynamique en SwiftUI
+
+Ce dépôt git contient le projet Taj Mahal commencé par Priyanka dans le cardre du parcours iOS.
+
+Faites un fork pour pouvoir le compléter.
+
+Happy learning !
---
+++ TajMahal/TajMahal/TajMahalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TajMahalApp.swift
+//  TajMahal
+//
+//  Created by Amandine Cousin on 31/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct TajMahalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
