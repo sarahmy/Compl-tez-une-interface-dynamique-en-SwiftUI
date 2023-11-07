ab706a84f2b11f4d76cb5fded4f98a168db49dfd
Amandine Cousin <cousin.amandine.pro@gmail.com>
2023-11-07 15:58:30 +0100

    Ajout du model de données qui contient la structure d'un plat du menu

--- TajMahal/TajMahal/Model.swift
+++ TajMahal/TajMahal/Model.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+// Cette enumération répertorie 3 niveaux de piment
+enum SpiceLevel {
+    case light
+    case medium
+    case hot
+}
+
+// Représente l'objet "plat", qui figure sur la carte du menu
+struct Dish {
+    var name: String
+    var description: String
+    var allergens: String
+    var ingredients: String
+    var spiceLevel: SpiceLevel
+}
