3f2c86cd5ea114928d1020b5abeac841b96f9fd8
Sarah Maimoun <maimoun.sarah@gmail.com>
2024-11-12 02:22:39 +0100

    mise à jour de la page d'accueil

---
+++ TajMahal/TajMahal/Views/LineView.swift
@@ -0,0 +1,18 @@
+//
+//  LineView.swift
+//  TajMahal
+//
+//  Created by Sarah Maimoun on 12/11/2024.
+//
+
+import SwiftUI
+
+struct LineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LineView()
+}
